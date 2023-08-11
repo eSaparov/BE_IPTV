8b5a1cce0c434e03c73cbf8f23a7f31b6976e95c
eSaparov <ezizataevich@gmail.com>
2023-08-11 12:54:38 +0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   BE_IPTV/movies/__pycache__/admin.cpython-311.pyc
    	modified:   BE_IPTV/movies/admin.py

--- BE_IPTV/movies/admin.py
+++ BE_IPTV/movies/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import *
-from django.contrib.auth.models import Group
+
 
 admin.site.register(User)
 admin.site.register(videos)
 admin.site.register(tracks)
 admin.site.register(appMetadata)
 admin.site.register(storedContent)
-admin.site.unregister(Group)
